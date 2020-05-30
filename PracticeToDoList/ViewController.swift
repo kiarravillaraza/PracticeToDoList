1f62c43871ba5641bdea2077e1f78d190e3051aa
Kiarra Villaraza <1010kiarra@gmail.com>
2020-05-29 19:19:23 -0500

    Initial Commit

---
+++ PracticeToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeToDoList
+//
+//  Created by Kiarra  on 5/29/20.
+//  Copyright © 2020 Kiarra . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
